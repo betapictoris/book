a7136a651e3d8466a07c01d7088cd0e198f5688b
Beta Pictoris <beta@ozx.me>
2023-05-06 11:46:36 -0700

    Added viewport

--- go.mod
+++ go.mod
@@ -9,23 +9,33 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/charmbracelet/bubbles v0.15.0 // indirect
+	github.com/charmbracelet/bubbletea v0.23.2 // indirect
 	github.com/charmbracelet/glamour v0.6.0 // indirect
 	github.com/charmbracelet/lipgloss v0.7.1 // indirect
 	github.com/charmbracelet/log v0.2.1 // indirect
-	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
+	github.com/knipferrc/teacup v0.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
+	github.com/muesli/ansi v0.0.0-20211031195517-c9f0611b6c70 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.3.4 // indirect
 	github.com/taylorskalyo/goreader v0.0.0-20220528130152-945e7448ceb5 // indirect
 	github.com/yuin/goldmark v1.5.4 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,12 +7,123 @@ import (
 	"github.com/charmbracelet/log"
 	"github.com/taylorskalyo/goreader/epub"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/charmbracelet/bubbles/viewport"
 	"github.com/charmbracelet/glamour"
+	"github.com/knipferrc/teacup/statusbar"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
 )
 
+const useHighPerformanceRenderer = false
+
 var text string
+
+// Bubble represents the properties of the UI.
+type Bubble struct {
+	statusbar statusbar.Bubble
+	viewport  viewport.Model
+	height    int
+	content   string
+	ready     bool
+
+	book *epub.Rootfile
+}
+
+// Init initializes the UI.
+func (Bubble) Init() tea.Cmd {
+	return nil
+}
+
+// NewStatusbar creates a new instance of the UI.
+func NewStatusbar() statusbar.Bubble {
+	sb := statusbar.New(
+		statusbar.ColorConfig{
+			Foreground: lipgloss.AdaptiveColor{Dark: "#ffffff", Light: "#ffffff"},
+			Background: lipgloss.AdaptiveColor{Light: "#F25D94", Dark: "#F25D94"},
+		},
+		statusbar.ColorConfig{
+			Foreground: lipgloss.AdaptiveColor{Light: "#ffffff", Dark: "#ffffff"},
+			Background: lipgloss.AdaptiveColor{Light: "#3c3836", Dark: "#3c3836"},
+		},
+		statusbar.ColorConfig{
+			Foreground: lipgloss.AdaptiveColor{Light: "#ffffff", Dark: "#ffffff"},
+			Background: lipgloss.AdaptiveColor{Light: "#3c3836", Dark: "#3c3836"},
+		},
+		statusbar.ColorConfig{
+			Foreground: lipgloss.AdaptiveColor{Light: "#ffffff", Dark: "#ffffff"},
+			Background: lipgloss.AdaptiveColor{Light: "#6124DF", Dark: "#6124DF"},
+		},
+	)
+
+	return sb
+}
+
+// Update handles all UI interactions.
+func (b Bubble) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		b.height = msg.Height
+
+		footerHeight := lipgloss.Height(b.footerView())
+		verticalMarginHeight := footerHeight
+
+		if !b.ready {
+			// Since this program is using the full size of the viewport we
+			// need to wait until we've received the window dimensions before
+			// we can initialize the viewport. The initial dimensions come in
+			// quickly, though asynchronously, which is why we wait for them
+			// here.
+			b.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			b.viewport.YPosition = 0
+			b.viewport.HighPerformanceRendering = useHighPerformanceRenderer
+			b.viewport.SetContent(b.content)
+			b.ready = true
+
+			// This is only necessary for high performance rendering, which in
+			// most cases you won't need.
+			//
+			// Render the viewport one line below the header.
+			b.viewport.YPosition = 1
+		} else {
+			b.viewport.Width = msg.Width
+			b.viewport.Height = msg.Height - verticalMarginHeight
+		}
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc", "q":
+			cmds = append(cmds, tea.Quit)
+		}
+	}
+
+	// Handle keyboard and mouse events in the viewport
+	b.viewport, cmd = b.viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return b, tea.Batch(cmds...)
+}
+
+// View returns a string representation of the UI.
+func (b Bubble) View() string {
+	if !b.ready {
+		return "\n  Initializing..."
+	}
+	return fmt.Sprintf("%s\n%s", b.viewport.View(), b.footerView())
+}
+
+func (b Bubble) footerView() string {
+	b.statusbar.SetSize(b.viewport.Width)
+	b.statusbar.SetContent("Book CLI", b.book.Title, "", fmt.Sprintf("%3.f%%", b.viewport.ScrollPercent()*100))
+	return b.statusbar.View()
+}
 
 func main() {
 	log.Info("Reading file...")
@@ -50,5 +161,13 @@ func main() {
 		log.Fatal("Failed to create glamour reader", "error", err)
 	}
 
-	fmt.Print(out)
+	p := tea.NewProgram(
+		Bubble{statusbar: NewStatusbar(), content: string(out), book: book},
+		tea.WithAltScreen(),       // use the full size of the terminal in its "alternate screen buffer"
+		tea.WithMouseCellMotion(), // turn on mouse support, so we can track the mouse wheel
+	)
+
+	if err := p.Start(); err != nil {
+		log.Fatal("could not run program:", err)
+	}
 }
